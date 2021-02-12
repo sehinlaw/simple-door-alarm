86dbd8857e58f588d20f4f66b91759b196fa0a51
Sehin <78458686+sehinlaw@users.noreply.github.com>
2021-02-12 18:10:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
 })
 basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) < 200) {
+    if (input.magneticForce(Dimension.Strength) < 150) {
         basic.showIcon(IconNames.Angry)
         music.playTone(988, music.beat(BeatFraction.Whole))
         music.playTone(880, music.beat(BeatFraction.Whole))
