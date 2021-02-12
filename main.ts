3f47cd92e10a91e2faf1e7d7f118a39fa13cb990
Sehin <78458686+sehinlaw@users.noreply.github.com>
2021-02-12 17:27:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    music.stopAllSounds()
+    basic.showIcon(IconNames.Happy)
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+})
+basic.forever(function () {
+    if (input.magneticForce(Dimension.Strength) < 200) {
+        basic.showIcon(IconNames.Angry)
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.playTone(880, music.beat(BeatFraction.Whole))
+    } else {
+        basic.showIcon(IconNames.Heart)
+    }
+})
